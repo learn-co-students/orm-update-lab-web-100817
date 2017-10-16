37ff57134de982b9e70042c46aebb8a58fb31b4f
Dakota Lillie <dakota.lillie@icloud.com>
2017-10-16 11:48:38 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -24,11 +24,7 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+    self.new(row[1], row[2], row[0])
   end
 
   def self.find_by_name(name)
@@ -52,7 +48,8 @@ class Student
     student
   end
 
-  attr_accessor :name, :grade, :id
+  attr_accessor :name, :grade
+  attr_reader :id
 
   def initialize(name=nil, grade=nil, id=nil)
     @name = name
